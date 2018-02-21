8931aca9e0006aa1237713405a6781459b0a0a7b
jadonn <jadonn@inmotionhosting.com>
2018-02-21 11:03:07 -0500

    Initial commit after preparing project for public publishing.

---
+++ README.md
@@ -0,0 +1,76 @@
+#Elasticsearch Import Script
+##Introduction
+This script provides a more convenient interface for getting data into [Elasticsearch](https://www.elastic.co/guide/en/elasticsearch/reference/current/index.html). The script was originally developed while researching Elasticsearch's potential uses for indexing various kinds of data. It was written to make the research more convenient and quicker; it was not ever intended to be a standalone project or product. However, maybe it will help someone else with importing data into Elasticsearch. :simple_smile:
+##Requirements
+The script requires Python. It was written in Python 2.7 (Sorry! Some workplaces have not made the jump to Python 3 yet.) and requires the excellent third-party libraries [requests](http://docs.python-requests.org/en/master/) and [Natural Language Toolkit](https://www.nltk.org/). This project does not provide a setup script at this time for installing these libraries.
+###Elasticsearch
+This script requires a running and reachable instance of Elasticsearch. Define the domain name or IP address for Elasticsearch as an environment variable called 'ES_HOST' before running the script. This can be done by either exporting the variable manually using the command line or by importing an environment file containing a declaration for ES_HOST.
+##Usage
+```
+python elasticsearch-file-importer.py --help
+usage: elasticsearch-file-importer.py [-h] {CSV,Logs,JSON} ...
+
+Read a data from a variety of file formats and post the data to Elasticsearch
+
+optional arguments:
+  -h, --help       show this help message and exit
+
+data_type:
+  Supported data format
+
+  {CSV,Logs,JSON}  Choose one of the supported data formats.
+    CSV            Import a CSV file into Elasticsearch
+    Logs           Import a log into ElasticSearch
+    JSON           Import a JSON file into Elasticsearch
+```
+###CSV Files
+When processing CSV files, the script first fetches the Elasticsearch mapping for the given index to determine which columns in the CSV file should be read and inserted into Elasticsearch documents. This functionality allows for only importing a subset of the columns from the CSV file instead of all of the columns. All of the columns can still be imported by defining an Elasticsearch index mapping with all of the columns.
+
+**The Elasticsearch index mapping's field names must exactly match the names of the columns in the CSV file for the columns to be imported.**
+
+The importer script will parse text fields into lists of keywords. The lists of keywords will be further analyzed using the [Natural Language Toolkit](https://www.nltk.org/) to generate bigrams, trigams, 4-grams, and 5-grams from the keywords. Bigrams, trigrams, 4-grams, and 5-grams are all kinds of n-grams. [n-grams](https://en.wikipedia.org/wiki/N-gram) are sequences of keywords that occur together in a given sample of text.
+
+If a stopWordsFile argument is provided, the script will analyze the list of keywords for each text field and remove the words found in the stop words file. [Stop words](https://en.wikipedia.org/wiki/Stop_words) are usually extremely common words that are removed from samples of text to help improve identifying the most meaningful words in a sample of text. Filtering stop words in particular can improve the quality of [tag cloud visualizations](https://www.elastic.co/guide/en/kibana/current/tagcloud-chart.html), such as those found in [Kibana](https://www.elastic.co/guide/en/kibana/current/introduction.html).
+```
+python elasticsearch-file-importer.py CSV --help
+usage: elasticsearch-file-importer.py CSV [-h] [--stopWordsFile STOPWORDSFILE]
+                                          csvFile esIndex
+
+positional arguments:
+  csvFile               Path to the CSV file to read
+  esIndex               Name of the Elasticsearch index mapping
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --stopWordsFile STOPWORDSFILE
+                        Path to a file of stopwords
+```
+###Logs
+The script will parse a log file line by line into JSON using a regular expression pattern to separate the components of the log format into named groups of characters. The regular expression pattern is defined in the first line of a separate file. An example of such a regular expression can be found in the regex-template.example file.
+**When parsing logs, the script will not fetch the Elasticsearch index mapping first. The script will read the log file and attempt to post the data to the supplied Elasticsearch index. The index does not have to be defined before hand, but defining an index mapping can sometimes provide better results when searching or aggregating indexed data.**
+```
+python elasticsearch-file-importer.py Logs --help
+usage: elasticsearch-file-importer.py Logs [-h] logFile formatFile esIndex
+
+positional arguments:
+  logFile     Path to the log file to read
+  formatFile  Path to file containing log format regex string.
+  esIndex     Name of the Elasticsearch index mapping
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
+###JSON
+The script will read a JSON file and post the data into Elasticsearch.
+**When readiong JSON, the script will not fetch the Elasticsearch index mapping first. The script will read the JSON file and attempt to post the data to the supplied Elasticsearch index. The index does not have to be defined before hand, but defining an index mapping can sometimes provide better results when searching or aggregating indexed data.**
+```
+python elasticsearch-file-importer.py JSON --help
+usage: elasticsearch-file-importer.py JSON [-h] jsonFile esIndex
+
+positional arguments:
+  jsonFile    Path to JSON file to read
+  esIndex     Name of the Elasticsearch index mapping
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
---
+++ elasticsearch-file-importer.py
@@ -0,0 +1,190 @@
+'''
+Importer script for reading data from CSV, log, and JSON files and posting it to Elasticsearch
+'''
+import csv
+import json
+import argparse
+import os
+import sys
+import re
+import requests
+
+from nltk.util import everygrams
+
+def process_report(args):
+    es_mapping_fields, es_mapping_name = retrieve_es_mapping_fields(args.esIndex)
+    try:
+        csv_data = read_csv_data(args.csvFile)
+    except IOError as error:
+        sys.exit('I/O error({0}): {1} {2}'.format(error.errno, error.strerror, args.csvFile))
+
+    bulk_row_data = ''
+    row_count = 0
+    create_action_string = '{"index": {}}'
+    for row in csv_data:
+        row_data = {}
+        text_tokens = []
+        for field in es_mapping_fields.keys():
+            if es_mapping_fields[field]['type'] == 'text':
+                text = row.get(field, None)
+                row_data[field] = text
+                if text:
+                    stopwords_file = args.stopWordsFile
+                    if stopwords_file:
+                        stopwords = set(line.strip() for line in open(stopwords_file))
+                        text_tokens = text.replace(':', ' ').replace(',', ' ').replace('.', ' ').replace(';', ' ').split()
+                        good_tokens = []
+                        for token in text_tokens:
+                            if token not in stopwords and token.lower() not in stopwords:
+                                good_tokens.append(token)
+                        text_tokens = good_tokens
+            elif 'Keywords' in field:
+                ngram_keywords = []
+                for length in range(1, 6):
+                    ngram_keywords.append([])
+                    for text_ngram in (list(everygrams(text_tokens, length, length))):
+                        keyword = ''
+                        for token in text_ngram:
+                            keyword = '%s %s' % (keyword, token)
+                        ngram_keywords[length - 1].append(keyword)
+                row_data[field] = {}
+                bigrams_field_name = "%sBigrams" % field
+                trigrams_field_name = "%sTrigrams" % field
+                fourgrams_field_name = "%sFourgrams" % field
+                fivegrams_field_name = "%sFivegrams" % field
+                row_data[field] = ngram_keywords[0]
+                row_data[bigrams_field_name] = ngram_keywords[1]
+                row_data[trigrams_field_name] = ngram_keywords[2]
+                row_data[fourgrams_field_name] = ngram_keywords[3]
+                row_data[fivegrams_field_name] = ngram_keywords[4]
+                text_tokens = []
+            else:
+                row_data[field] = row.get(field, None)
+        row_data_string = json.dumps(row_data)
+        bulk_row_data = '%s\n%s\n%s\n' % (bulk_row_data, create_action_string, row_data_string)
+        row_count += 1
+        if row_count == 10000:
+            print 'Sending 10000 records to Elasticsearch'
+            send_data_to_elasticsearch(bulk_row_data, args.esIndex, es_mapping_name)
+            bulk_row_data = ''
+            row_count = 0
+        
+    send_data_to_elasticsearch(bulk_row_data, args.esIndex, es_mapping_name)
+    
+def send_data_to_elasticsearch(data, es_index, es_mapping_name):
+    try:
+        es_host = os.environ['ES_HOST']
+    except KeyError:
+        sys.exit('ES_HOST environment variable has not been defined.')
+    post_request_url = 'http://%s:9200/%s/%s/_bulk' % (es_host, es_index, es_mapping_name)
+    post_request = requests.post(post_request_url, headers={'Content-Type': 'application/x-ndjson'}, data=data)
+    if(post_request.status_code == 200):
+        print 'Current batch of data inserted successfully.'
+
+def retrieve_es_mapping_fields(es_index):
+    try:
+        es_host = os.environ['ES_HOST']
+    except KeyError:
+        sys.exit('ES_HOST environment variable has not been defined.')
+    mapping_request_url = 'http://%s:9200/%s/_mapping' % (es_host, es_index)
+    mapping_request = requests.get(mapping_request_url)
+    if(mapping_request.status_code == 404):
+        error_message = 'Mapping for index %s could not be found.' % es_index
+        sys.exit(error_message)
+    elif(mapping_request.status_code != 200):
+        error_message = 'There was some error when requesting the mapping.'
+        sys.exit(error_message)
+    else:
+        es_mapping = mapping_request.json()[es_index]['mappings']
+        es_mapping_name = es_mapping.keys()[0]
+        es_mapping_fields = es_mapping[es_mapping.keys()[0]]['properties']
+        return es_mapping_fields, es_mapping_name
+
+def read_csv_data(csv_file_path):
+
+    csv_file = open(csv_file_path, 'r')
+    reader = csv.DictReader(csv_file)
+    return reader
+
+def process_log(args):
+    log_file_path = args.logFile
+    format_file_path = args.formatFile
+    es_index = args.esIndex
+    try:
+        log_file = open(log_file_path, 'r')
+    except IOError:
+        file_not_found_message = 'No file found at %s. Please enter a valid file path.' % log_file_path
+        sys.exit(file_not_found_message)
+    try:
+        format_file = open(format_file_path, 'r')
+    except IOError:
+        file_not_found_message = 'No file found at %s. Please enter a valid file path.' % format_file_path
+        sys.exit(file_not_found_message)
+    log_format = format_file.read()
+    pattern = re.compile(log_format)
+    parsed_lines = []
+    for log_line in log_file:
+        parsed_line = pattern.match(log_line)
+        if parsed_line:
+            parsed_lines.append(parsed_line.groupdict())
+        else:
+            print "Log line did not match pattern: %s" % log_line
+        if len(parsed_lines) >= 1000:
+            save_parsed_lines(parsed_lines, es_index)
+            parsed_lines = []
+    if len(parsed_lines) > 0:
+        save_parsed_lines(parsed_lines, es_index)
+
+def save_parsed_lines(parsed_lines, es_index):
+    print "Saving batch of parsed lines."
+    prepare_simple_json_bulk_import(parsed_lines, es_index, 'log_entry')
+
+def process_json(args):
+    json_file_path = args.jsonFile
+    es_index = args.esIndex
+    try:
+        json_file = open(json_file_path, 'r')
+    except IOError:
+        file_not_found_message = 'No file found at %s. Please enter a valid file path.' % json_file_path
+        sys.exit(file_not_found_message)
+    json_data = json.load(json_file)
+    save_json_data(json_data, es_index)
+
+def save_json_data(json_data, es_index):
+    print "Saving JSON data."
+    prepare_simple_json_bulk_import(json_data, es_index, 'entry')
+
+def prepare_simple_json_bulk_import(json_data, es_index, es_mapping):
+    bulk_entry_string = ''
+    create_action_string = '{"index": {}}'
+    for entry in json_data:
+        entry_data_string = json.dumps(entry)
+        bulk_entry_string = '%s\n%s\n%s\n' % (bulk_entry_string, create_action_string, entry_data_string)
+    send_data_to_elasticsearch(bulk_entry_string, es_index, es_mapping)
+
+if __name__ == '__main__':
+
+    PARSER = argparse.ArgumentParser(
+        description='Read a data from a variety of file formats and post the data to Elasticsearch'
+        )
+    
+    SUBPARSERS = PARSER.add_subparsers(title="data_type", description="Supported data format", help="Choose one of the supported data formats.")
+    CSV_PARSER = SUBPARSERS.add_parser("CSV", help="Import a CSV file into Elasticsearch")
+    CSV_PARSER.add_argument('csvFile', help='Path to the CSV file to read')
+    CSV_PARSER.add_argument('esIndex', help='Name of the Elasticsearch index mapping')
+    CSV_PARSER.add_argument('--stopWordsFile', help='Path to a file of stopwords')
+    CSV_PARSER.set_defaults(func=process_report)
+
+    LOG_PARSER = SUBPARSERS.add_parser("Logs", help="Import a log into ElasticSearch")
+    LOG_PARSER.add_argument("logFile", help="Path to the log file to read")
+    LOG_PARSER.add_argument("formatFile", help="Path to file containing log format regex string.")
+    LOG_PARSER.add_argument("esIndex", help="Name of the Elasticsearch index mapping")
+    LOG_PARSER.set_defaults(func=process_log)
+
+    JSON_PARSER = SUBPARSERS.add_parser("JSON", help="Import a JSON file into Elasticsearch")
+    JSON_PARSER.add_argument("jsonFile", help="Path to JSON file to read")
+    JSON_PARSER.add_argument("esIndex", help="Name of the Elasticsearch index mapping")
+    JSON_PARSER.set_defaults(func=process_json)
+
+    ARGS = PARSER.parse_args()
+    ARGS.func(ARGS)
